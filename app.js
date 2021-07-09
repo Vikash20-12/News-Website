0395492453de8a9d53633f19717d6e0099097940
Vikash20-12 <85476256+Vikash20-12@users.noreply.github.com>
2021-07-09 11:01:36 +0530

    changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 
 const app = express();
@@ -6,15 +7,19 @@ const PORT = 5000;
 
 //Static files
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/css', express.static(path.join(__dirname,'public/css')));
 
 //templating engine
-app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname,'./src/views'));
 
 //routes
 const mainRoute = require('./src/route/news');
 app.use('/', mainRoute);
+
+app.get('/',(req, res)=>{
+    res.render(news);
+})
 
 //Server listening on port
 app.listen(PORT, ()=> console.log(`server listening on port ${PORT}`));
